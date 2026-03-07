483d1fac3d71903db2d29fdeb545fde56ead3e33
whyatul <atulmaurya18.04@gmail.com>
2026-03-07 19:42:56 +0530

    feat(frontend): create Home page with dashboard overview

---
+++ pos-frontend/src/pages/Home.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from "react";
+import BottomNav from "../components/shared/BottomNav";
+import Greetings from "../components/home/Greetings";
+import { BsCashCoin } from "react-icons/bs";
+import { GrInProgress } from "react-icons/gr";
+import MiniCard from "../components/home/MiniCard";
+import RecentOrders from "../components/home/RecentOrders";
+import PopularDishes from "../components/home/PopularDishes";
+
+const Home = () => {
+
+    useEffect(() => {
+      document.title = "POS | Home"
+    }, [])
+
+  return (
+    <section className="bg-[#1f1f1f]  h-[calc(100vh-5rem)] overflow-hidden flex gap-3">
+      {/* Left Div */}
+      <div className="flex-[3]">
+        <Greetings />
+        <div className="flex items-center w-full gap-3 px-8 mt-8">
+          <MiniCard title="Total Earnings" icon={<BsCashCoin />} number={512} footerNum={1.6} />
+          <MiniCard title="In Progress" icon={<GrInProgress />} number={16} footerNum={3.6} />
+        </div>
+        <RecentOrders />
+      </div>
+      {/* Right Div */}
+      <div className="flex-[2]">
+        <PopularDishes />
+      </div>
+      <BottomNav />
+    </section>
+  );
+};
+
+export default Home;
